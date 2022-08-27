47db39f1b7a54113bc2a9cd9f6a497af2e0d4d2f
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-28 00:09:40 +0300

    cоздать и вывести в консоль значения нового массива содержащие минимальное и максимальное значение каждой строки.

---
+++ Array_55/Program.cs
@@ -0,0 +1,56 @@
+﻿//Array_55
+
+//Дана двумерный массив n*m заполненный значениями с клавиатуры.
+//Создать и вывести в консоль значения нового массива содержащие 
+//минимальное и максимальное значение каждой строки.
+
+
+Console.WriteLine("Введите размер матрицы по горизонтали: ");
+string inpCols = Console.ReadLine();
+Console.WriteLine("Введите размер матрицы по вертикали: ");
+string inpRows = Console.ReadLine();
+if (inpCols != "" || inpRows != "")
+{
+    if (int.TryParse(inpCols, out int colums) && int.TryParse(inpRows, out int rows))
+    {
+        if (colums > 0 && rows > 0)
+        {
+            int[,] array = new int[rows, colums];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine($"Введите для {i} сроки {colums} элементов:");
+                try
+                {
+                    int[] row = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                    for (int j = 0; j < colums; j++)
+                        array[i, j] = row[j];
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка ввода. Не корректная строка!");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            int[,] extremums = new int[rows, 2];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int min = array[i, 0], max = array[i, 0];
+                for (int j = 1; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] < min)
+                        min = array[i, j];
+                    if (array[i, j] > max)
+                        max = array[i, j];
+                }
+                Console.WriteLine($"Строка №{i}: мin = {min};\tmax = {max}");
+                extremums[i, 0] = min;
+                extremums[i, 1] = max;
+            }
+        }
+        else Console.WriteLine("Длина массива не может быть отрицательной!");
+    }
+    else Console.WriteLine("Ошибка ввода. Не корректное число!");
+}
+else Console.WriteLine("Ошибка ввода. Пустая строка.");
+Console.ReadKey();
