2ff4a2b87290aed373c83328ed5a5da9e84040a1
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-27 21:41:49 +0300

    заполнить массив чередованием значений 0 и 1

---
+++ Array_44/Program.cs
@@ -0,0 +1,36 @@
+﻿//Array_44
+//Дан пустой двумерный массив размеров n*n. 
+//Заполнить массив чередованием значений 0 и 1, в следующем виде:
+
+//0   1   0   1   0
+//0   1   0   1   0
+//0   1   0   1   0
+//0   1   0   1   0
+//0   1   0   1   0
+
+
+Console.WriteLine("Введите размер квадратной матрицы: ");
+string input = Console.ReadLine();
+if (input != "")
+{
+    if (int.TryParse(input, out int size))
+    {
+        if (size > 0)
+        {
+            Console.WriteLine("Полученный массив: ");
+            int[,] array = new int[size, size];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = j % 2;
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        else Console.WriteLine("Длина массива не может быть отрицательной!");
+    }
+    else Console.WriteLine("Ошибка ввода. Не корректное число!");
+}
+else Console.WriteLine("Ошибка ввода. Пустая строка.");
